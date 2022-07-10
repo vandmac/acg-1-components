341a087f7c96ee442c44708fd4b5a549800290f8
LGGRAMAM\Andrzej <vandmac@gmail.com>
2022-07-10 14:04:31 +0200

    finish debugging section training

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  servers;
+  servers=[]; //wrong: it was: servers;
 
   onAddServer() {
     this.servers.push('Another Server');
   }
 
   onRemoveServer(id: number) {
-    const position = id + 1;
+    const position = id; //wrong: it was: id+1
     this.servers.splice(position, 1);
   }
 }

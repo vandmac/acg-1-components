de72b29e07a0ab819514183e9839dcbafd4328aa
LGGRAMAM\Andrzej <vandmac@gmail.com>
2022-07-10 08:49:05 +0200

    commit project start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css'],
-  styles: [`
-  h3{
-    color:dodgerblue;
-    font-style:italic;
-  }
-  `]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
 }

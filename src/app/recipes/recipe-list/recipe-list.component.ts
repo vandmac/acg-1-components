191047f5de397e96962e7ebfdad121b484a741d8
LGGRAMAM\Andrzej <vandmac@gmail.com>
2022-07-11 13:56:14 +0200

    pass recipe data with property binding lec.87

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Recipe } from '../recipe.model';
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"],
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('Pizza Romantica', 'Very tasty and easy to prepare', 'https://pixabay.com/get/gf82444e1bce150b574fdf1801d72af6f1f8b10a5162dc6f26f550e8ec3eb185d911aa4037dbb8f9ef189ebf2735ac800_640.jpg'),
-    new Recipe('Cheese Soup', 'Half an hour and You get it ready', 'https://pixabay.com/get/g7f8d7199a406fd80fdc791f91748b6d2c7ea3063908654a075d53a2efa0e7a30fe45c035763d35f9553d04e18e332b36_640.jpg')
+    new Recipe(
+      "Pizza Romantica",
+      "Very tasty and easy to prepare",
+      "https://pixabay.com/get/g348850f0beb318e6325fa2a845421dc6e79d402963046cfb6fbb7cfd974cefc0c7cc15ab6ce9660b85f5e16d21c1c709_640.jpg"
+    ),
+    new Recipe(
+      "Cheese Soup",
+      "Half an hour and You get it ready",
+      "https://pixabay.com/get/g6d50e8d2bd54771f5dcd5c589b1288354f26cbf706755b360b0400f2f32e2372fbd6a5f6ac42f796225980559bcd0645_640.jpg"
+    ),
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
